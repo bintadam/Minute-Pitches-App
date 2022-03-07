5c8c2319361fbd83ce0ddb4ba9196984fcdab98f
zakiya adam <zakiyaadan12@gmail.com>
2022-03-07 22:56:29 +0300

    initialezed migrate class

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
-from flask_script import 
+from xmlrpc.client import Server
+from flask_script import Manager, Server 
+from  flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
+from 
 
 
-#....
+#create instance manage
+manager = Manager(app)
+manager.add_command('server', Server)
+
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
