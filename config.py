7b4dbbd72314f78ca42995d31cdb898ffece251c
zakiya adam <zakiyaadan12@gmail.com>
2022-03-08 15:02:32 +0300

    install flask-email

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ class Config:
     SECRET_KEY = 'zakiya'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://zakiya:hooyokoraad1234@localhost/MINUTES-PITCHES'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
