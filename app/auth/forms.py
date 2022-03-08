4953f948c47156d2e96d115c2550823794e45b74
zakiya adam <zakiyaadan12@gmail.com>
2022-03-08 13:01:36 +0300

    added booleanfield input field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, ValidationError
+from wtforms import StringField,PasswordField,BooleanField, SubmitField, ValidationError
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
@@ -9,6 +9,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    remember = BooleanField('Remember me')
     submit = SubmitField('Sign Up')
 
 
