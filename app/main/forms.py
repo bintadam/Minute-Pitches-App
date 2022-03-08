a3b71c7103cb21a936ae6928eef6d1d898faedcd
zakiya adam <zakiyaadan12@gmail.com>
2022-03-08 14:15:28 +0300

    created form classupdatedprofile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField,ValidationError
-from wtforms.validators import DataRequired
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
