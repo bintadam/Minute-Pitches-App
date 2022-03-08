30330f182f408334d8c0ab606aa78a5da4810613
zakiya adam <zakiyaadan12@gmail.com>
2022-03-08 14:05:26 +0300

    created route handled by profile view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
-from flask_login import login_required
+from flask import render_template,request,redirect,url_for,abort
+from ..models import Reviews, User
 
 
 
-@main.route('/pitch//new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_pitch(id):
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
