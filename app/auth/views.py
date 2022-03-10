23d86dffd613863e684ed5c6f3b79734a32ee017
zakiya adam <zakiyaadan12@gmail.com>
2022-03-11 01:32:53 +0300

    fixed errors

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,7 @@ def login():
             login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or Password')
-    return render_template('auth/login.html', loginform = form)
+    return render_template('auth/login.html', LoginForm = form)
 
 
 
@@ -24,16 +24,16 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        user.save_u()
+       user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+       user.save_u()
 
-        mail_message("Welcome to Minute-Pitches","email/welcome_user",user.email,user=user)
+       mail_message("Welcome to Minute-Pitches","email/welcome_user",user.email,user=user)
  
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)    
+       db.session.add(user)
+       db.session.commit()
+       return redirect(url_for('auth.login'))
+       title = "New Account"
+    return render_template('auth/register.html', RegistrationFormF = form)    
 
 
 
