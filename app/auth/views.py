f54c44443f45c40c135c1e3e03eec2d7cc07246c
zakiya adam <zakiyaadan12@gmail.com>
2022-03-11 01:41:43 +0300

    fixed errors

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,16 +7,19 @@ from ..email import mail_message
 from . import auth
 
 
-@auth.route('/login', methods = ['GET','POST'])
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
-        if user != None and user.verify_password(form.password.data):
-            login_user(user,form.remember.data)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = LoginForm.email.data).first()
+        if user is not None and user.verify_password(LoginForm.password.data):
+            login_user(user,LoginForm.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
+
         flash('Invalid username or Password')
-    return render_template('auth/login.html', LoginForm = form)
+
+    title = "Minutes-Pitches | login"
+    return render_template('auth/login.html',LoginForm = LoginForm,title=title)
 
 
 
@@ -24,16 +27,12 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-       user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-       user.save_u()
-
-       mail_message("Welcome to Minute-Pitches","email/welcome_user",user.email,user=user)
- 
-       db.session.add(user)
-       db.session.commit()
-       return redirect(url_for('auth.login'))
-       title = "New Account"
-    return render_template('auth/register.html', RegistrationFormF = form)    
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user.save_u()
+        mail_message("Welcome to Minute-Pitches","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',RegistratioForm = form)    
 
 
 
